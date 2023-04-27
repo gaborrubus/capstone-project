5ebf7386cc947d031320f9317590241d9f7e030d
Gabor Szederkenyi <gaborszederkenyi@MacBook-Air-von-Gabor.local>
2023-04-27 14:41:25 +0200

    safety commit

---
+++ components/AllActivities.js
@@ -0,0 +1,12 @@
+import { activities } from "@/db/users/activities/activities";
+import styled from "styled-components";
+import Link from "next/link";
+
+export default function AllActivities({ id, name, description }) {
+  return (
+    <>
+      <p>name: {activities.name}</p>
+      <p>description: {activities.description}</p>
+    </>
+  );
+}
---
+++ components/StyledBox.js
@@ -0,0 +1,8 @@
+import styled, { css } from "styled-components";
+
+export const StyleBox = styled.div`
+  border: solid black;
+  border-radius: 25px;
+  margin-top: 1rem;
+  margin-left: 1rem;
+`;
